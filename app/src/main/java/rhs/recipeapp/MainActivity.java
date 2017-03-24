b32ead7527c9e7c9500c723c8fdf0334459f5e66
Rachel <rdejaen@gmail.com>
2017-03-24 09:35:59 -0700

    connected the button to the next activity

--- app/src/main/java/rhs/recipeapp/MainActivity.java
+++ app/src/main/java/rhs/recipeapp/MainActivity.java
@@ -2,6 +2,10 @@ package rhs.recipeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+
     }
 
     //Is Github actually working?!
