76e4e2b77a2ea2576689b39b3b07fa21cb1febd3
Rachel <rdejaen@gmail.com>
2017-04-17 09:04:28 -0700

    made browse activity screen, pops up after you click on the browse button

--- app/src/main/java/rhs/recipeapp/WelcomeActivity.java
+++ app/src/main/java/rhs/recipeapp/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package rhs.recipeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button browse = (Button) findViewById(R.id.browse); //creates the button to continue to the next activity
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToBrowseActivity();
+            }
+        });
+    }
+
+    private void goToBrowseActivity() {
+        Intent intent = new Intent(this, BrowseActivity.class);
+        startActivity(intent);
+
     }
 }
