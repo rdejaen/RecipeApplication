8ca0e651ecfc8f1a8fcebf2c546103592f3010e8
Rachel <rdejaen@gmail.com>
2017-04-17 09:25:54 -0700

    added the coresponding activities to the buttons from the welcome page

--- app/src/main/java/rhs/recipeapp/WelcomeActivity.java
+++ app/src/main/java/rhs/recipeapp/WelcomeActivity.java
@@ -19,11 +19,62 @@ public class WelcomeActivity extends AppCompatActivity {
                 goToBrowseActivity();
             }
         });
+
+        Button categories = (Button)findViewById(R.id.categories);
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCatActivity();
+            }
+        });
+
+        Button tips = (Button)findViewById(R.id.tips);
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToTipsActivity();
+            }
+        });
+
+        Button favs = (Button)findViewById(R.id.favs);
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFavsActivity();
+            }
+        });
+
+        Button settings = (Button)findViewById(R.id.settings);
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSettingsActivity();
+            }
+        });
     }
 
     private void goToBrowseActivity() {
         Intent intent = new Intent(this, BrowseActivity.class);
         startActivity(intent);
+    }
 
+    private void goToCatActivity() {
+        Intent intent = new Intent(this, CatActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToTipsActivity() {
+        Intent intent = new Intent(this, TipsActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToFavsActivity() {
+        Intent intent = new Intent(this, FavsActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
